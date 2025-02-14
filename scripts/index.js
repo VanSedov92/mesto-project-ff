f97cc106dab554ac34ebc38bd0cfaf66bc57c061
Ivan Sedov <van.sedov92@gmail.com>
2025-02-14 15:34:36 +0300

    функция удаления карточек и добавление alt картинкам

--- scripts/index.js
+++ scripts/index.js
@@ -7,39 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-
-const addCardButton = document.querySelector('.profile__add-button');
-const deleteCardButton = document.querySelectorAll('.card__delete-button');
-const cardsList = document.querySelector('.places__list');
-const cardTemplate = document.querySelector('#card-template').content;
-
+ 
+const cardsList = document.querySelector('.places__list');   
+const cardTemplate = document.querySelector('#card-template').content; 
+ 
+// Функция создания карточки
 function createCard(item) {
-    const cardsItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const cardsItem = cardTemplate.querySelector('.places__item').cloneNode(true); 
     cardsItem.querySelector('.card__image').src = item.link;
+    cardsItem.querySelector('.card__image').alt = `Изображение места: ${item.name}`; 
     cardsItem.querySelector('.card__title').textContent = item.name;
-    cardsItem.querySelector('.card__delete-button').addEventListener('click', function () {
-        cardsItem.remove();
-    })
-    return cardsItem;
-  };
+    const deleteCardButton = cardsItem.querySelector('.card__delete-button');
+    deleteCardButton.addEventListener('click', function() {
+      deleteCard(cardsItem)
+    });
+    return cardsItem; 
+  }; 
 
-function addCards() {
-    initialCards.forEach(function (item) {
-        const itemToRender = createCard(item);
-        cardsList.append(itemToRender);
-    })
-};
-addCards();
+// Функция удаления карточки
 
-let i = 0;
-function addOneCard() {
-    if (i < initialCards.length) {
-      const item = createCard(initialCards[i]);
-      cardsList.append(item);
-      i++;
-    } else {
-      i = 0;
-    }
-};
+function deleteCard(cardsItem) {
+  cardsItem.remove();
+}
 
-addCardButton.addEventListener('click', addOneCard);
+// Выводим карточки на страницу 
+
+function addCards() { 
+    initialCards.forEach(function (item) { 
+        const renderCard = createCard(item, deleteCard); 
+        cardsList.append(renderCard); 
+    }) 
+}; 
+addCards(); 
+
